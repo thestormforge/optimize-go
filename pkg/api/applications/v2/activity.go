8c60f4155a59181be1ef860bbe67995ff9243edf
Jeremy Gustie <jeremy@stormforge.io>
2022-01-31 13:15:52 -0500

    API for creating approve and refresh activities

--- pkg/api/applications/v2/activity.go
+++ pkg/api/applications/v2/activity.go
@@ -81,8 +81,10 @@ func (q *ActivityFeedQuery) SetType(t ...string) {
 
 type Activity struct {
 	api.Metadata `json:"-"`
-	Run          *RunActivity  `json:"run,omitempty"`
-	Scan         *ScanActivity `json:"scan,omitempty"`
+	Run          *RunActivity     `json:"run,omitempty"`
+	Scan         *ScanActivity    `json:"scan,omitempty"`
+	Approve      *ApproveActivity `json:"approve,omitempty"`
+	Refresh      *RefreshActivity `json:"refresh,omitempty"`
 }
 
 type RunActivity struct {
@@ -92,6 +94,16 @@ type RunActivity struct {
 
 type ScanActivity struct {
 	Scenario string `json:"scenario"`
+	ActivityFailure
+}
+
+type ApproveActivity struct {
+	Recommendation string `json:"recommendation"`
+	ActivityFailure
+}
+
+type RefreshActivity struct {
+	Application string `json:"application"`
 	ActivityFailure
 }
 
